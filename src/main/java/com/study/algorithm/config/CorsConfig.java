eb38791b7bc6fdc38580d0862e5b9152cec2b771
Quiltix <vanya200632@gmail.com>
2025-05-18 15:10:03 +0300

    feat: finish all

--- src/main/java/com/study/algorithm/config/CorsConfig.java
+++ src/main/java/com/study/algorithm/config/CorsConfig.java
@@ -1,12 +1,13 @@
 package com.study.algorithm.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
-
+@Configuration
 public class CorsConfig {
 
     @Bean
